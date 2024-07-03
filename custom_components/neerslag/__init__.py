e1cae20dbb5434afb587317ba340179c85c5a545
rrooggiieerr <rogier+github@staveren.net>
2024-07-04 00:10:00 +0200

    Fixes non async call async_forward_entry_setup

--- custom_components/neerslag/__init__.py
+++ custom_components/neerslag/__init__.py
@@ -50,10 +50,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     hass_data["unsub_options_update_listener"] = unsub_options_update_listener
     hass.data[DOMAIN][config_entry.entry_id] = hass_data
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, platform)
-        )
+    await hass.config_entries.async_forward_entry_setup(config_entry, PLATFORMS)
 
     return True
 
